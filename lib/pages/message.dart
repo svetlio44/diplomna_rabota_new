b3cd2d4d90f46fa108ca2686f672ae08fadf6e1b
svetlio44 <99183797+svetlio44@users.noreply.github.com>
2025-03-25 15:05:53 +0200

    къси неща

--- lib/pages/message.dart
+++ lib/pages/message.dart
@@ -7,7 +7,6 @@
   Copyright and Good Faith Purchasers © 2021-present initappz.
 */
 import 'package:flutter/material.dart';
-import 'package:diplomna_rabota_new/pages/chat.dart';
 import '../components/styles.dart';
 
 class Notifications extends StatefulWidget {
@@ -20,6 +19,29 @@ class Notifications extends StatefulWidget {
 }
 
 class _NotificationsState extends State<Notifications> {
+  List<Map<String, String>> notifications = [
+    {
+      'title': 'New Bid',
+      'description': 'Someone has placed a new bid on your ads.',
+      'time': '5 minutes ago'
+    },
+    {
+      'title': 'Item Liked',
+      'description': 'Someone liked your ads.',
+      'time': '10 minutes ago'
+    },
+    {
+      'title': 'New Message',
+      'description': 'You have a new message.',
+      'time': '15 minutes ago'
+    },
+    {
+      'title': 'New Bid',
+      'description': 'Someone',
+      'time': '20 minutes ago'
+    }
+  ];
+
   @override
   void initState() {
     super.initState();
@@ -30,9 +52,9 @@ class _NotificationsState extends State<Notifications> {
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
-          actions: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.more_horiz)),
-          ],
+          // actions: [
+          //   IconButton(onPressed: () {}, icon: Icon(Icons.more_horiz)),
+          // ],
           backgroundColor: Colors.white,
           iconTheme: IconThemeData(color: Colors.black54),
           elevation: 0,
@@ -40,131 +62,19 @@ class _NotificationsState extends State<Notifications> {
           title: Text('Notifications',
               style: TextStyle(color: Colors.black, fontFamily: 'medium')),
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _buildNotitfication('Vivian', 'liked your posts'),
-                    _buildNotitfication('Mary', 'comment on your posts'),
-                    _buildNotitfication('Vivian', 'liked your 4 posts'),
-                  ],
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 16),
-                  child: greyText('Friends Request'),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _buildRequest('Cortana'),
-                    _buildRequest('Jarvis'),
-                    _buildRequest('Alexa'),
-                  ],
-                ),
-              ],
-            ),
-          ),
+        body: ListView.builder(
+          itemCount: notifications.length,
+          itemBuilder: (context, index) {
+            final notification = notifications[index];
+            return Card(
+              margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              child: ListTile(
+                title: Text(notification['title'] ?? 'No title'),
+                subtitle: Text(notification['description'] ?? 'No description'),
+                trailing: Text(notification['time'] ?? 'No time'),
+              ),
+            );
+          },
         ));
-  }
-
-  Widget _buildNotitfication(name, text) {
-    return GestureDetector(
-      onTap: () {
-      },
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 8),
-        margin: EdgeInsets.only(bottom: 16),
-        decoration: BoxDecoration(
-          border: Border(bottom: BorderSide(color: Colors.black26)),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              padding: EdgeInsets.only(left: 8, top: 8, bottom: 10),
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/37.jpg'),
-                radius: 26,
-              ),
-            ),
-            SizedBox(width: 10),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(children: [
-                    boldText(name),
-                    SizedBox(width: 4),
-                    greyText(text)
-                  ]),
-                  SizedBox(height: 6),
-                  greyTextSmall('30 mins ago'),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildRequest(name) {
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 8),
-        margin: EdgeInsets.only(bottom: 16),
-        decoration: BoxDecoration(
-          border: Border(bottom: BorderSide(color: Colors.black26)),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              padding: EdgeInsets.only(left: 8, top: 8, bottom: 10),
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/37.jpg'),
-                radius: 26,
-              ),
-            ),
-            SizedBox(width: 10),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(children: [
-                    boldText(name),
-                    SizedBox(width: 4),
-                    greyText('Followed you')
-                  ]),
-                  SizedBox(height: 6),
-                  greyTextSmall('30 mins ago'),
-                ],
-              ),
-            ),
-            OutlinedButton(
-              child: greyTextSmall("Follow"),
-              style: OutlinedButton.styleFrom(
-                foregroundColor: appColorBtn, side: BorderSide(
-                color: appColor,
-              ),
-                backgroundColor: Colors.white,
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(50),
-                  ),
-                ),
-              ),
-              onPressed: () {},
-            ),
-          ],
-        ),
-      ),
-    );
   }
 }
